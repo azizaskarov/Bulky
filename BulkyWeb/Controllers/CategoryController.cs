7a1163ecc4458351fe7fffe0d3cf87bcce76ff89
azizaskarov <azizasqarov773@gmail.com>
2024-07-09 20:58:42 +0500

    update

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+public class CategoryController : Controller
+{
+    private readonly AppDbContext dbContext;
+
+    public CategoryController(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public IActionResult Index()
+    {
+        var categories = dbContext.Categories.ToList();
+        return View(categories);
+    }
+}
--- BulkyWeb/Data/AppDbContext.cs
+++ BulkyWeb/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data;
 
@@ -7,5 +8,16 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
 
+    }
+
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category {Id =1,Name = "Action", DisplayOrder = 1},
+            new Category {Id =2,Name = "SciFi", DisplayOrder = 2},
+            new Category {Id =3,Name = "History", DisplayOrder = 3}
+                );
     }
 }
